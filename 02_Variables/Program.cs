360bb464132da2e39e6446ede236e80800e2bf25
Sertaç <sertac54ozdemir@gmail.com>
2024-12-01 14:57:43 +0300

    gnc

--- 02_Variables/Program.cs
+++ 02_Variables/Program.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _02_Variables
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
